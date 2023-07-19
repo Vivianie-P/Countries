1ca3bc9c696b58481d49c5a684889981c611c671
Vivianie-P <vivianieprice@gmail.com>
2023-07-18 22:36:41 -0700

    Woking on building out country card component

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,21 @@
 const CountryCard = () => {
-	return <div>Country Card</div>;
+	return (
+		<div>
+			<div className="m-auto mt-20 h-[360px] w-[300px] rounded-xl bg-white ">
+				{/* <img src="" alt="country flag" /> */}
+				<h1 className="flex text-4xl font-extrabold text-black">Germany</h1>
+				<h2 className="flex items-center  text-left text-4xl font-bold text-black">
+					Population: <span className="">894202490</span>
+				</h2>
+				<h2 className="flex text-left text-4xl font-bold text-black">
+					Region: <span className="">Europe</span>
+				</h2>
+				<h2 className="flex text-left text-4xl font-bold text-black">
+					Captial: <span className="">Berlin</span>
+				</h2>
+			</div>
+		</div>
+	);
 };
 
 export default CountryCard;
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,91 @@
+// import downArrow from "../assets/images/chevron-down.svg";
+import { useState } from "react";
+
 function Filter() {
+	const [toggler, setToggler] = useState(false);
+
+	const handleButtonClick = () => {
+		setToggler(!toggler);
+	};
+
 	return (
-		<div>
-			<button className="mt-8 h-16 w-64 rounded-lg bg-white">
+		<div className="relative">
+			<button
+				id="dropdownDefaultButton"
+				className="mt-16 inline-flex h-20 w-72 items-center rounded-lg bg-white px-5 py-2.5 text-center text-2xl font-medium text-black"
+				type="button"
+				onClick={handleButtonClick}
+			>
 				Filter by Region
+				<svg
+					className="ml-2.5 h-2.5 w-2.5"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 10 6"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="m1 1 4 4 4-4"
+					/>
+				</svg>
 			</button>
+
+			{toggler && (
+				<div
+					id="dropdown"
+					className="z-10 w-72 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+				>
+					<ul
+						className="mt-2 w-72 rounded-lg bg-white py-2 text-sm text-black"
+						aria-labelledby="dropdownDefaultButton"
+					>
+						<li>
+							<a
+								href="#"
+								className="block px-4 py-2 text-2xl hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								Africa
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								className="block px-4 py-2 text-2xl hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								America
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								className="block px-4 py-2 text-2xl hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								Asia
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								className="block px-4 py-2 text-2xl hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								Europe
+							</a>
+						</li>
+						<li>
+							<a
+								href="#"
+								className="block px-4 py-2 text-2xl hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>
+								Oceania
+							</a>
+						</li>
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
